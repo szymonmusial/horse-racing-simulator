e503fcc1a181f2b55ae9424ca017a4e09c039c94
Szymon Musiał <szymonmusial.sm@gmail.com>
2026-02-25 08:42:34 +0100

    chore: add Tailwind CSS and uuid

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="welcome-view">
-    <h1>Welcome to the Horse Racing Simulator</h1>
+    <h1 class="text-2xl font-bold underline">Welcome to the Horse Racing Simulator</h1>
   </div>
 </template>
 <script setup lang="ts"></script>
